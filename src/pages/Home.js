6651c2e50c0ed8da64c6775501ff305935e2229a
squal94 <lounis.khadir@wanadoo.fr>
2023-10-22 15:08:09 +0200

    Home init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>MARVEL</h1>
+    <div className="home">
+      <div className="home__left">
+        <h1 className="home__left--title">
+          <span>MARVEL</span> <br /> UNIVERS
+        </h1>
+      </div>
+      <div className="home__right"></div>
     </div>
   );
 };
