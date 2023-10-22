8647b05012960d5cd3600c68860df1f20ca16162
squal94 <lounis.khadir@wanadoo.fr>
2023-10-22 16:48:04 +0200

    Diaporama v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,12 @@
 import React from "react";
+import blackWidow from "./../assets/img/img1.png";
+import captainMarvel from "./../assets/img/img2.png";
+import hulk from "./../assets/img/img3.png";
+import blackPanther from "./../assets/img/img4.png";
+import ironMan from "./../assets/img/img5.png";
+import spiderMan from "./../assets/img/img6.png";
+import thor from "./../assets/img/img7.png";
+import captainAmerica from "./../assets/img/img8.png";
 
 const Home = () => {
   return (
@@ -8,7 +16,21 @@ const Home = () => {
           <span>MARVEL</span> <br /> UNIVERS
         </h1>
       </div>
-      <div className="home__right"></div>
+      <div className="home__right">
+        <div className="home__right__diaporama">
+          <div className="home__right__diaporama--wrapper">
+            <img src={blackWidow} alt="Black widow" />
+            <img src={hulk} alt="hulk" />
+            <img src={blackPanther} alt="Black Panther" />
+            <img src={ironMan} alt="Iron man" />
+            <img src={spiderMan} alt="Spider man" />
+            <img src={thor} alt="Thor" />
+            <img src={captainMarvel} alt="Captain marvel" />
+            <img src={captainAmerica} alt="Captain America" />
+            <img src={blackWidow} alt="Black widow" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
